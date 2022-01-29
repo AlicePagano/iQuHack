283963f47f89064e53a1169ae819fef302a8ff81
AlicePagano <alicepagano0797@gmail.com>
2022-01-29 23:59:23 +0100

    add new data with more samples

--- main.py
+++ main.py
@@ -7,26 +7,35 @@ import os
 if not os.path.exists('data/'):
     os.makedirs('data/')
 
-num_reps = 8
+# Define theta range
 num_theta = 200
-
 theta = np.linspace(0,2*np.pi,num_theta,endpoint=False)
+
+# Number of algorithm execution for each theta
+num_times = 20
+
+# Number of error-correcting block repetition
+num_reps = 8
+
 all_data = []
-
 for tt in range(num_theta):
-
     print('Counter:', tt)
-    qc = QuantumCircuit(5)
-    encode_psi(qc, theta=theta[tt])
 
-    for ii in range(num_reps):
-        apply_check(qc, ii)
+    syndromes_list = []
+    for nn in range(num_times):
+        qc = QuantumCircuit(5)
+        encode_psi(qc, theta=theta[tt])
 
-    qc.measure_all()
+        for ii in range(num_reps):
+            apply_check(qc, ii)
 
-    job = execute(qc, backend=backend, basis_gates=basis_set, coupling_map=coupling, shots=1024)
-    counts = job.result().get_counts()
-    occurrences, syndromes = decode_outputs(counts)
-    all_data.append({'theta': theta[tt], 'syndromes': syndromes})
+        qc.measure_all()
 
-np.save('data/data_num_reps=8.npy', np.array(all_data) )
+        job = execute(qc, backend=backend, basis_gates=basis_set, coupling_map=coupling, shots=1024)
+        counts = job.result().get_counts()
+        occurrences, syndromes = decode_outputs(counts)
+        syndromes_list.append(syndromes)
+    
+    all_data.append({'theta': theta[tt], 'all_syndromes': syndromes_list})
+
+np.save('data/num_reps=8_num_times=20.npy', np.array(all_data) )
