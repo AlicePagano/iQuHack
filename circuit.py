a64a96ab7856d7db1adafe1fb4be97fed6f9e239
Marco Ballarin <marco97.ballarin@gmail.com>
2022-01-30 00:02:12 +0100

    Added circuit for phase-flip errors

--- circuit.py
+++ circuit.py
@@ -1,9 +1,11 @@
 from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
 import numpy as np
 
-def encode_psi(circ, theta=0, reset=True):
+def encode_psi(circ, theta=0, bit_flip=True):
     """Encode the logical state |psi(theta)> on a quantum circuit qc,
-    where |psi> = 0.5*( (1+e^{i\theta})|0> + (1-e^{i\theta})|1> )
+    where |psi> = 0.5*( (1+e^{i\theta})|0> + (1-e^{i\theta})|1> ).
+    Instead, if looking for phase-flip errors the state is:
+    |psi> = 0.707*( |0> + e^{i\theta}|1> )
     
 
     Parameters
@@ -12,30 +14,40 @@ def encode_psi(circ, theta=0, reset=True):
         qiskit quantum circuit
     theta : float
         Angle of rotation along z
-    reset : bool, optional
-        Flag to apply the reset. If True apply them. Default to True.
+    bit_flip : bool, optional
+        Flag to choose the type of error to correct.
+        If True correct bit-flips, if False phase-flips. Default to True.
     """
     creg = ClassicalRegister(1, 'initialization')
     circ.add_register(creg)
     
-    if reset:
-        for ii in range(5):
-            circ.reset(ii)
+    for ii in range(5):
+        circ.reset(ii)
 
     circ.h(2)
     circ.rz(theta, 2)
     circ.h(2)
-    for ii in range(5):
-        if ii==2:
-            continue
-        circ.cx(2, ii)
+    if bit_flip:
+        for ii in range(5):
+            if ii==2:
+                continue
+            circ.cx(2, ii)
 
-    circ.h(2)
-    circ.cz(0, 2)
+        circ.h(2)
+        circ.cz(0, 2)
+    else:
+        for ii in range(5):
+            if ii==2:
+                continue
+            circ.cz(2, ii)
+
+        circ.h(2)
+        circ.cx(0, 2)
+
     circ.measure(2, creg[0])
     circ.barrier()
 
-def apply_check(circ, check_num):
+def apply_check(circ, check_num, bit_flip=True):
     """Apply a check on the quantum circuit
 
     Parameters
@@ -44,6 +56,9 @@ def apply_check(circ, check_num):
         qiskit quantum circuit
     check_num : int
         Number of the check
+    bit_flip : bool, optional
+        Flag to choose the type of error to correct.
+        If True correct bit-flips, if False phase-flips. Default to True.
     """
     if not isinstance(circ, QuantumCircuit):
         raise TypeError()
@@ -56,12 +71,22 @@ def apply_check(circ, check_num):
 
     circ.add_register(creg)
     
-    circ.cx(ctrls[0], 2)
-    circ.cx(ctrls[1], 2)
+    if bit_flip:
+        circ.cx(ctrls[0], 2)
+        circ.cx(ctrls[1], 2)
+    else:
+        circ.cz(ctrls[0], 2)
+        circ.cz(ctrls[1], 2)
     circ.measure(2, creg[0])
     
-    circ.cx(ctrls[1], 2)
-    circ.cx(ctrls[2], 2)
+    if bit_flip:
+        circ.cx(ctrls[1], 2)
+        circ.cx(ctrls[2], 2)
+    else:
+        circ.h(2)
+        circ.cz(ctrls[1], 2)
+        circ.cz(ctrls[2], 2)
+        circ.h(2)
     circ.measure(2, creg[1])
 
 def decode_outputs(counts):
