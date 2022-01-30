0139212285b971924f7c9ed2e519bb7781ecd5c1
marco trenti <40923963+kafka42@users.noreply.github.com>
2022-01-30 13:21:09 +0100

    Update dnn_predict.py

--- src/dnn_predict.py
+++ src/dnn_predict.py
@@ -4,9 +4,19 @@ import numpy as np
 """
 predict and parse output of dnn model.
 
-input data is a len = 10 list or array
+input data is a len = 10 + 1 list or array of states 
+string (like "00", "01", ..)
 """
 
+# Encode measurements outcomes
+def parse_input(data):
+    out_data = []
+    stati = ["00", "01", "10","11"]
+    for me in data[0].split(" ")[::-1]:
+        out_data.append(stati.index(str(me)))
+   return out_data
+
+# Predict sample
 def dnn_predict(model, sample):
     pred = model.predict(sample)
     cat_pred = np.zeros_like(pred)
